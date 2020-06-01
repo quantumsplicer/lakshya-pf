a4bf48c2bf1cff9e7d8087bcc89ded738e91d08b
Maanav Seth <manuseth30@gmail.com>
2020-06-01 23:13:53 +0530

    Updates

---
+++ precache-manifest.1b8d793639893630c503272d5f309e86.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112b9dd0225f1645df24979a390f9f23",
+    "url": "/lakshya-pf/index.html"
+  },
+  {
+    "revision": "f2a00d811c781ee6977c",
+    "url": "/lakshya-pf/static/css/main.cc78e3d2.chunk.css"
+  },
+  {
+    "revision": "e07afd8ba17ecd759bb1",
+    "url": "/lakshya-pf/static/js/2.96029673.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/lakshya-pf/static/js/2.96029673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2a00d811c781ee6977c",
+    "url": "/lakshya-pf/static/js/main.081e8645.chunk.js"
+  },
+  {
+    "revision": "9a3fadb6d54fee6ae687",
+    "url": "/lakshya-pf/static/js/runtime-main.91f288b3.js"
+  },
+  {
+    "revision": "850510385ceb63ff90c9fa8f5c7405bc",
+    "url": "/lakshya-pf/static/media/behance.85051038.svg"
+  },
+  {
+    "revision": "23397c6118cf6b79b1e3e2f7af7f2b9e",
+    "url": "/lakshya-pf/static/media/dribbble.23397c61.svg"
+  },
+  {
+    "revision": "4e95d935cfa7c6de3e2cb679a3fa6dbf",
+    "url": "/lakshya-pf/static/media/friends1.4e95d935.png"
+  },
+  {
+    "revision": "621c199f3b096ca191be8595971adf37",
+    "url": "/lakshya-pf/static/media/friends2.621c199f.png"
+  },
+  {
+    "revision": "351cbdee7b0965c6d4d7e82591b68269",
+    "url": "/lakshya-pf/static/media/friends3.351cbdee.png"
+  },
+  {
+    "revision": "d67533924706edb24747d70a1f084743",
+    "url": "/lakshya-pf/static/media/friends4.d6753392.png"
+  },
+  {
+    "revision": "a5cc80954bf8d44f74acfe58236bbd09",
+    "url": "/lakshya-pf/static/media/lakshya.a5cc8095.png"
+  },
+  {
+    "revision": "c040e76dc10bfb243beaaf673bbcff69",
+    "url": "/lakshya-pf/static/media/linkedin.c040e76d.svg"
+  },
+  {
+    "revision": "42733f33bbb3569754b4a24c629bb760",
+    "url": "/lakshya-pf/static/media/logo.42733f33.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lakshya-pf/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86627eadb96c685b6d03273562e2966f",
+    "url": "/lakshya-pf/static/media/project2.86627ead.png"
+  },
+  {
+    "revision": "790760ccab2e87db809a61df93dda4c2",
+    "url": "/lakshya-pf/static/media/project3.790760cc.png"
+  },
+  {
+    "revision": "84f29c24c651ba6c34a015bcd451156f",
+    "url": "/lakshya-pf/static/media/project4.84f29c24.png"
+  },
+  {
+    "revision": "59d2f937acbf2e8ed988d991e93f86a8",
+    "url": "/lakshya-pf/static/media/project5.59d2f937.png"
+  }
+]);
